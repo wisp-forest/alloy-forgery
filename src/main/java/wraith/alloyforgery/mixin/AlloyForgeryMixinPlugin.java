5d239bd603c7ec25c1d0b0d818f8e14af135fa1d
glisco <glisco03@web.de>
2021-11-11 19:26:54 +0100

    lithium compat for hoppers, fixes #27

---
+++ src/main/java/wraith/alloyforgery/mixin/AlloyForgeryMixinPlugin.java
@@ -0,0 +1,49 @@
+package wraith.alloyforgery.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class AlloyForgeryMixinPlugin implements IMixinConfigPlugin {
+
+    private static final boolean LITHIUM_LOADED = FabricLoader.getInstance().isModLoaded("lithium");
+
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return LITHIUM_LOADED || !mixinClassName.contains("HopperHelperMixin");
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
