d06f8b49d095642bb29f533b97f27fc59498a3dc
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-05 17:38:34 -0500

    [1.19.3] EMI Plugin (#87) from fzzyhmstrs
    * add EMI dependency
    
    * add EMI version in props
    
    * add EMI entrypoint in mod json
    
    * update forge_controller.png with new bits needed for widgets; added emi lang category; added translation key for cycling button (REI and EMI)
    
    * add translatable text to cycling button widget instead of hardcoded Text.of
    
    * added EMI plugin
    
    * adding transfer handler for auto-transferring

---
+++ src/main/java/wraith/alloyforgery/compat/emi/CustomSlotWidget.java
@@ -0,0 +1,26 @@
+package wraith.alloyforgery.compat.emi;
+
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.widget.SlotWidget;
+
+public class CustomSlotWidget extends SlotWidget {
+
+    public CustomSlotWidget(EmiIngredient stack, int x, int y, EmiRecipe recipe) {
+        super(stack, x, y);
+        this.mutableStack = stack;
+        this.output = true;
+        this.recipeContext(recipe);
+    }
+
+    private EmiIngredient mutableStack;
+
+    public void setMutableStack(EmiIngredient stack){
+        this.mutableStack = stack;
+    }
+
+    @Override
+    protected EmiIngredient getStack() {
+        return mutableStack;
+    }
+}
