3f780fcb2f975916c346ff1fb373cb97a5ee6e45
NatIrisa <sweetofficialv2@gmail.com>
2023-06-23 22:36:34 +0800

    Attempting to restore EMI compat in 1.20
    reverts disabling EMI on #108

--- src/main/java/wraith/alloyforgery/compat/emi/AlloyForgeryEmiPlugin.java
+++ src/main/java/wraith/alloyforgery/compat/emi/AlloyForgeryEmiPlugin.java
@@ -1,9 +1,9 @@
 package wraith.alloyforgery.compat.emi;
 
-//import dev.emi.emi.api.EmiPlugin;
-//import dev.emi.emi.api.EmiRegistry;
-//import dev.emi.emi.api.recipe.EmiRecipeCategory;
-//import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiStack;
 import net.minecraft.block.Block;
 import net.minecraft.util.Identifier;
 import wraith.alloyforgery.AlloyForgery;
@@ -12,22 +12,22 @@ import wraith.alloyforgery.recipe.AlloyForgeRecipe;
 
 public class AlloyForgeryEmiPlugin /*implements EmiPlugin*/ {
 
-//    private static final Identifier FORGE_ID = AlloyForgery.id("alloy_forge");
-//    public static final EmiRecipeCategory FORGE_CATEGORY = new EmiRecipeCategory(FORGE_ID, EmiStack.of(ForgeRegistry.getControllerBlocks().get(0)));
-//
-//
-//    @Override
-//    public void register(EmiRegistry registry) {
-//        registry.addCategory(FORGE_CATEGORY);
-//
-//        for (Block controller : ForgeRegistry.getControllerBlocks()) {
-//            registry.addWorkstation(FORGE_CATEGORY, EmiStack.of(controller));
-//        }
-//
-//        for (AlloyForgeRecipe recipe : registry.getRecipeManager().listAllOfType(AlloyForgeRecipe.Type.INSTANCE)) {
-//            registry.addRecipe(new AlloyForgeryEmiRecipe(recipe));
-//        }
-//
-//        registry.addRecipeHandler(AlloyForgery.ALLOY_FORGE_SCREEN_HANDLER_TYPE, new AlloyForgeryEmiRecipeHandler());
-//    }
+    private static final Identifier FORGE_ID = AlloyForgery.id("alloy_forge");
+    public static final EmiRecipeCategory FORGE_CATEGORY = new EmiRecipeCategory(FORGE_ID, EmiStack.of(ForgeRegistry.getControllerBlocks().get(0)));
+
+
+    @Override
+    public void register(EmiRegistry registry) {
+        registry.addCategory(FORGE_CATEGORY);
+
+        for (Block controller : ForgeRegistry.getControllerBlocks()) {
+            registry.addWorkstation(FORGE_CATEGORY, EmiStack.of(controller));
+        }
+
+        for (AlloyForgeRecipe recipe : registry.getRecipeManager().listAllOfType(AlloyForgeRecipe.Type.INSTANCE)) {
+            registry.addRecipe(new AlloyForgeryEmiRecipe(recipe));
+        }
+
+        registry.addRecipeHandler(AlloyForgery.ALLOY_FORGE_SCREEN_HANDLER_TYPE, new AlloyForgeryEmiRecipeHandler());
+    }
 }
