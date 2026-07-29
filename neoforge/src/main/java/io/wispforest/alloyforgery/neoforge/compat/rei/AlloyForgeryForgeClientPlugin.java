fd557c065f1ad3c1a54e0b9bda75a53442daeb8d
Noaaan <noaaan@hotmail.com>
2026-07-29 14:47:42 +0200

    Remove REI plugin class

--- neoforge/src/main/java/io/wispforest/alloyforgery/neoforge/compat/rei/AlloyForgeryForgeClientPlugin.java
+++
@@ -1,9 +0,0 @@
-package io.wispforest.alloyforgery.neoforge.compat.rei;
-
-import io.wispforest.alloyforgery.compat.rei.AlloyForgeryClientPlugin;
-import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
-import me.shedaniel.rei.forge.REIPluginClient;
-
-@REIPluginClient
-public class AlloyForgeryForgeClientPlugin extends AlloyForgeryClientPlugin implements REIClientPlugin {
-}
